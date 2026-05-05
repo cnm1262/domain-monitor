03fb81ab15f41738087d8f24a520ba88a2b0bc13
chaimae tarre <tarrechaimae5@gmail.com>
2026-05-05 15:06:06 +0100

    fix email crash

--- email_utils.py
+++ email_utils.py
@@ -1,25 +1,22 @@
 import smtplib
 from email.message import EmailMessage
+import os
 
-EMAIL_ADDRESS = "your_email@gmail.com"
-EMAIL_PASSWORD = "your_app_password"
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 def send_alert(to_email, domain):
-    msg = EmailMessage()
-    msg["Subject"] = "🚨 Website Down Alert"
-    msg["From"] = EMAIL_ADDRESS
-    msg["To"] = to_email
+    try:
+        msg = EmailMessage()
+        msg["Subject"] = "🚨 Website Down Alert"
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = to_email
 
-    msg.set_content(f"""
-Bonjour,
+        msg.set_content(f"Your website {domain} is DOWN.")
 
-Votre site web est indisponible :
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
 
-{domain}
-
-Merci de le vérifier rapidement.
-""")
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        smtp.send_message(msg)
+    except Exception as e:
+        print("Email error:", e)
